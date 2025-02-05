f658507ceb2a0eb9f5aa646ed62544c8e7cc975a
OWL4C <124401812+OWL4C@users.noreply.github.com>
2025-02-05 22:00:51 +0100

    Finalised build script, adding License information and Comment for alternate linuxdeploy usage.

--- AppImageBuild.sh
+++ AppImageBuild.sh
@@ -1,11 +1,20 @@
 #!/bin/zsh
+
+
+# SPDX-License-Identifier: GPL-3.0-or-later or MIT or 0BSD
+
+#### THIS IS USING MANUAL APPIMAGETOOL. I HAVENT FOUND ANY ISSUES WITH THIS IN ITS CURRENT FORM; BUT ALTERNATIVELY LINUXDEPLOY COULD BE USED LIKE THIS
+# ./linuxdeploy-x86_64.AppImage --appdir AppDir --executable dist/release/blenderlauncher --output appimage -d dist/release/AppImageDir/blenderlauncher.desktop -i dist/release/AppImageDir/bl_256.png
+#
+#### THIS DOES NOT ADD ANY METADATA AND IS PROBABLY NOT FIT TO BE DISTRIBUTED ON APPIMAGE MARKETPLACES, BUT FOR USAGE LOCALLY DOWNLOADED AS AN EXECUTABLE, IT SHOULD BE FINE
+
 # 1. Build BL
 
-#sh scripts/build_linux.sh
+sh scripts/build_linux.sh
 executable='dist/release/Blender Launcher'
 
 # 2. Download AppImage Tool (ENSURE YOU HAVE ZSYNC INSTALLED)
-#wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
+wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
 chmod +x appimagetool-x86_64.AppImage
 
 # 3. Create AppImage FS
@@ -31,16 +40,18 @@ Categories=Graphics;3DGraphics
 EOF
 
 # 5. Create AppRun
-cat > $AID/AppRun << EOF
+#HERE="$(dirname "$(readlink -f "${0}")")"
+#cat > $AID/AppRun << EOF
 #!/bin/bash
-exec '$APPDIR/usr/bin/BlenderLauncher'
-EOF
+#exec '$HERE/usr/bin/BlenderLauncher'
+#EOF
+ln -s usr/bin/BlenderLauncher $AID/AppRun
 ## 5.5 Make it executable
 chmod +x $AID/AppRun
 
 # 6. Create AppImage with AppImagetool (and zsync)
 ARCH=x86_64 ./appimagetool-x86_64.AppImage $AID BlenderLauncher.AppImage
 
-
-
-
+# 7. Cleanup
+rm appimagetool-x86_64.AppImage
+rm -rf $AID
